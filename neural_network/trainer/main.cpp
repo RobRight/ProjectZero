1a024c4e4685934d7ca2e95b8a709d13541ce05a
William <wgregory@nevada.unr.edu>
2016-10-15 23:34:25 -0700

    update

--- neural_network/trainer/main.cpp
+++ neural_network/trainer/main.cpp
@@ -1,13 +1,9 @@
 
+// Run Network Trainer for Neural Network
+
 #include <iostream>
 
 #include "network_trainer.h"
-
-// 0:trainer
-// 1:gridspace
-// 2:rover
-
-int run_type = 0;
 
 int main()
 {
@@ -16,20 +12,5 @@ int main()
 		Trainer::Trainer t;
 		t.train();
 	}
-	/*
-	else if (run_type == 1)
-	{
-		Gridspace::Grid g;
-		// dimentions (x, y)
-		// player (human:true, bot:false)
-		g.play_game(10, 10, true);
-	}
-	else if (run_type == 2)
-	{
-		Rover::Rover r;
-		r.rw.create_world(20, 20);
-		r.add_rover(2, 1, 45);
-	}
-	*/
 	return 0;
 }
