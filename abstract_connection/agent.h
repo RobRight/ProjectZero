174ffb48f3da7f6450c0fa4e35fdf6da6c167be3
William <wgregory@nevada.unr.edu>
2016-10-22 10:39:27 -0700

    update

--- abstract_connection/agent.h
+++ abstract_connection/agent.h
@@ -1,6 +1,27 @@
 
+/*
+
+Agent Abstract Class
+Copyright (c) 2016 William Gregory.  All rights reserved.
+
+Agents give an action and recieve a state.  (reward?)
+
+*/
+
+#include <vector>
+
 class Agent {
-    give_action();
-    get_state();
-    get_reward();
-}
+private:
+    std::vector <double> action;
+public:
+    std::vector <double> give_action(void) {
+        return action;
+    }
+    void get_state(std::vector <double> in) {
+
+    }
+    //void get_reward(std::vector <double> in) {
+    //
+    //}
+};
+
--- abstract_connection/domain.h
+++ abstract_connection/domain.h
@@ -1,7 +1,26 @@
+/*
 
+Agent Abstract Class
+Copyright (c) 2016 William Gregory.  All rights reserved.
+
+Domains recieve an action and give a state.  (reward?)
+
+*/
+
+#include <double>
 
 class Domain {
-    get_action();
-    give_state();
-    give_reward():
-}
+private:
+    std::vector <double> state;
+    //std::vector <double reward;
+public:
+    void get_action(std::vector <double> in) {
+
+    }
+    std::vector <double> give_state(void) {
+        return state;
+    }
+    //std::vector <double> give_reward(void) {
+    //    return reward;
+    //}
+};
