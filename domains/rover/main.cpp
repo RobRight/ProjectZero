acb66b5d3a5ae8ba3df4bd4bd86135728fcd05ad
William <wgregory@nevada.unr.edu>
2016-10-22 05:45:19 -0700

    add new rover class. ready to test

--- domains/rover/main.cpp
+++ domains/rover/main.cpp
@@ -3,8 +3,32 @@
 
 #include "rover.h"
 
+// - settings -
+// world entities
+unsigned int pois = 1;      // POIs to generate at start
+unsigned int rovers = 1;    // rovers to generate at start
+// world size (min default to zero)
+unsigned int world_x = 6;   // X max
+unsigned int world_y = 6;   // Y max
+// - settings end - 
+
+std::vector <double> states;
+std::vector <unsigned int> moves;
+
 int main()
 {
+    RD::RoverDomain rd(world_x, 0, world_y, 0);
+    rd.setup(pois, rovers);
+
+    /*
+    states = rd.return_states();
+    std::cout << "states:" std::endl;
+    for (std::size_t i=0; i<states.size(); ++i)
+        std::cout << "\t" << states.at(i) << std::endl;
+    std::cout << std::endl;
+
+    if(rd.set_moves(moves)) std::cout << "moves set" << std::endl;
+    */
 
     return 0;
 }
--- domains/rover/rover.h
+++ domains/rover/rover.h
@@ -1,134 +1,272 @@
+
 /*
 
-Rover Class
+Rover Domain
+Copyright (c) 2016 William Gregory.  All rights reserved.
 
-Copyright C 2016 William.  All rights reserved.
-
-Description:
-- rover in a world
-- add, move, ...
-
-Todo:
-- change to polar quardinates
+Notes:
+- dir (1:N, 2:E, 3:S, 4:W)
+- loc (x,y)
+- pos_y (N+,S-); pos_x (E+,W-)
 
 */
 
-#ifndef _ROVER_CLASS_
-#define _ROVER_CLASS_
+#ifndef _ROVER_DOMAIN_
+#define _ROVER_DOMAIN_
 
 #include <vector>
+#include <stdlib>
+#include <time.h>
 
-namespace Rover {
+#define LYRAND (double)rand()/RAND_MAX
 
-	struct rover {
-		// position
-		unsigned int x;		// position X (-:+)
-		unsigned int y;		// position Y (-:+)
-		unsigned int dir;	// direction (0:359)
-		// properties
-		unsigned int speed;	// movement speed (1:10)
-		unsigned int type;	// rover type (1:basic, 2:pro)??
-	};
 
-	struct world {
-		unsigned int x;
-		unsigned int y;
-		// .at(row).at(col);
-		// .at(y).at(x);
-		std::vector < std::vector <unsigned int> > w;
-	};
+namespace RD {
 
-	class Rover_world
-	{
-	private:
-		world w;
-	public:
-		Rover_world();
-		unsigned int type_null;
-		unsigned int type_rover;
-		std::vector <rover> rovers;
-		void create_world(unsigned int, unsigned int);
-	};
+    // location, grid based
+    struct loc {
+        //double r_mag;  // distance from origin
+        //double theta;  // angle
+        int pos_x;
+        int pos_y;
+    };
 
-	Rover_world::Rover_world()
-	{
-		type_null = 0;
-		type_rover = 1;
-	}
+    // position of intrest
+    //
+    // Functions:
+    // - return_position
+    // - set_position
+    //
+    class POI {
+    private:
+        loc position;
+        double value;
+    public:
+        POI(loc in_loc,
+            double in_val) {
+            // setup
+            position = loc;
+            value = in_val;
+            movable = false;
+        }
+        // return position
+        loc return_position() {
+            return position;
+        }
+        // set position
+        bool set_position(loc in) {
+            if (movable) position = in;
+            else return false;
+            return true;
+        }
+    }
 
-	void Rover_world::create_world(unsigned int in_x, unsigned int in_y)
-	{
-		w.x = in_x;
-		w.y = in_y;
-		for (std::size_t j = 0; j < w.y; ++j)
-		{
-			std::vector <unsigned int> t_row;
-			for (std::size_t j = 0; j < w.y; ++j)
-			{
-				t_row.push_back(type_null);
-			}
-			w.w.push_back(t_row);
-		}
-	}
 
-	//////////////////////////////////
-	class Rover
+    // rover object
+    // 
+    // ToDo:
+    // - add sensors (state)
+    //
+    // Functions:
+    // - return_position
+    // - set_position
+    // - generate_pos_given_move
+    // - check_state *
+    // - return_state *
+    // 
+    class Rover {
+    private:
+        /// variables
+        loc position;  // rover position
+        //unsigned int type;
+        double state; // MORE ON THIS
+        /// functions
+        // check state (sensors)
+        void check_state() {
+            state = 1;
+        }
+    public:
+        Rover(loc in_loc) {
+            position = in_loc;
+        }
+        // return position
+        loc return_position() {
+            return position;
+        }
+        // set position
+        bool set_position(loc in) {
+            position = in;
+            return true;
+        }
+        // return position given move from current position
+        loc generate_pos_given_move(unsigned int dir) {
+            loc t_pos = position;
+            switch(dir) {
+                case 1:  // N
+                    t_pos.pos_y += 1;
+                    break;
+                case 2:  // E
+                    t_pos.pos_x += 1;
+                    break;
+                case 3:  // S
+                    t_pos.pos_y -= 1;
+                    break;
+                case 4:  // W
+                    t_pos.pos_x -= 1;
+                    break;
+                default:
+                    // ERROR
+            }
+            return t_pos;
+        }
+        // return state (sensors)
+        double return_state() {
+            check_state();
+            return state;
+        }
+    };
 
-	{
-	public:
-		Rover_world rw;
-		Rover();
-		bool check_position_clear(unsigned int, unsigned int);
-		rover generate_rover(const unsigned int&, const unsigned int&, const unsigned int&);
-		bool add_rover(unsigned int, unsigned int, unsigned int);
-		bool move_rover(unsigned int, unsigned int);
-	};
 
-	Rover::Rover()
-	{
-
-	}
-
-	// check if given position contains a rover
-	bool Rover::check_position_clear(unsigned int in_x, unsigned int in_y)
-	{
-		bool t_clear = true;
-		for (std::size_t i = 0; i < rw.rovers.size(); ++i)
-		{
-			if (rw.rovers.at(i).x == in_x)
-				if (rw.rovers.at(i).y == in_y)
-					t_clear = false;
-		}
-		return t_clear;
-	}
-
-	rover Rover::generate_rover(const unsigned int& in_x, const unsigned int& in_y, const unsigned int& in_dir)
-	{
-		rover r;
-		r.x = in_x;
-		r.y = in_y;
-		r.dir = in_dir;
-		r.speed = 1;
-		r.type = 1;
-		return r;
-	}
-
-	bool Rover::add_rover(unsigned int in_x, unsigned int in_y, unsigned int in_dir)
-	{
-		bool t_placed = false;
-		if (check_position_clear(in_x, in_y))
-		{
-			rw.rovers.push_back(generate_rover(in_x, in_y, in_dir));
-		}
-		return t_placed;
-	}
-
-	bool Rover::move_rover(unsigned int in_x, unsigned int in_y)
-	{
-		bool t_moved = true;
-
-		return t_moved;
-	}
+    // rover domain
+    //
+    // Functions:
+    // - generate_random_position
+    // - add_random_pois
+    // - add_random_rovers
+    // - check_pos_clear
+    // - add_rover
+    // - move_rover
+    // - add POI
+    // ----------------
+    // - Rover
+    // - setup
+    // - return states
+    // - set moves
+    //
+    class RoverDomain {
+    private:
+        /// variables
+        std::vector <POI> pois;  // all POIs
+        std::vector <Rover> rovers;  // all rovers
+        // world ranges
+        int pos_x_max;
+        int pos_x_min;
+        int pos_y_max;
+        int pos_y_min;
+        /// functions
+        // generate random position (sub)
+        loc generate_random_position() {
+            loc l;
+            while (true) {
+                l.pos_x = rand() / (pos_x_max-pos_x_min) + pos_x_min;
+                l.pos_y = rand() / (pos_y_max-pos_x_min) + pos_y_min;
+                if (check_pos_clear(l)) break;
+            }
+            return l;
+        }
+        // generate random POSs (sub)
+        void add_random_pois(unsigned int in_count) {
+            for (std::size_t i=0; i<in_count; ++i) {
+                double poi_value = 1.0;
+                loc l = generate_random_position();
+                POI p(l, poi_value);
+                pois.push_back(p);
+            }
+        }
+        void add_random_rovers(unsigned int in_count) {
+            for (std::size_t i=0; i<in_count; ++i) {
+                loc l = generate_random_position();
+                Rover r(l);
+                rovers.push_back(r);
+            }
+        }
+        // return true if position is clear of rovers and POIs (sub)
+        bool check_pos_clear(loc in) {
+            // rovers
+            for (std::size_t i=0; i<rovers; ++i) {
+                if (in.pos_x == rovers.at(i).pos_x) {
+                    if (in.pos_y == rovers.at(i).pos_y) {
+                        return false;
+                    }
+                }
+            }
+            // POIs
+            for (std::size_t i=0; i<POIS; ++i) {
+                if (in.pos_x == POIS.at(i).pos_x) {
+                    if (in.pos_y == POIS.at(i).pos_y) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        // add rover to world if space is clear (sub)
+        bool add_rover(double in_x, in_y) {
+            loc l;
+            l.pos_x = in_x;
+            l.pos_y = in_y;
+            if (!check_pos_clear(l)) return false;
+            Rover r(l);
+            rovers.push_back(r);
+            return true;
+        }
+        // check if move is legal (sub)
+        bool move_rover(Rover in_r, unsigned int in_dir) {
+            loc t_pos = in_r.generate_pos_given_move(in_dir);
+            if (!check_pos_clear(t_pos)) return false;
+            in_r.set_position(t_pos);
+            return true;
+        }
+        // move all rovers given all locations (sub)
+        void move_single_rover_master(Rover in_r, unsigned int in_d) {
+            loc t_pos = in_r.generate_pos_given_move(in_d);
+            in_r.set_position(t_pos);
+        }
+        // move all rovers given all locations (sub)
+        void move_all_rovers_master(std::vector <unsigned int> in) {
+            for (std::size_t i=0; i<rovers.size(); ++i) {
+                loc t_pos = in_r.generate_pos_given_move(in.at(i));
+                rovers.at(i).set_position(t_pos);
+            }
+        }
+        // --------------------------------------------------
+        public:
+        RoverDomain(int in_x_max,
+                    int in_x_min,
+                    int in_y_max,
+                    int in_y_min) {
+            // setup
+            pos_x_max = in_x_max;
+            pos_x_min = in_x_min;
+            pos_y_max = in_y_max;
+            pos_y_min = in_y_min;
+            srand(time(null));
+        }
+        // setup (main)
+        // add randomly placed POIS and rovers to the world
+        void setup(unsigned int in_pois, unsigned int in_rovers) {
+            add_random_pois(in_pois);
+            add_random_rovers(in_rovers);
+        }
+        // return states (main)
+        std::vector <double> return_states() {
+            std::vector <double> t_states;
+            for (std::size_t i=0; i<rovers.size(); ++i) {
+                t_states.push_back(rovers.at(i).return_state());
+            }
+            return t_states;
+        }
+        // set moves (main)
+        bool set_moves(std::vector <unsigned int> in) {
+            if (in.size() != rovers.size()) return false;
+            std::vector <loc> t_locs;
+            for (std::size_t i=0; i<rovers.size(); ++i) {
+                if(!move_rover(rovers.at(i), in.at(i))) return false;
+            }
+            move_all_rovers_master(in);
+            return true;
+        }
+    };
 
 }
 
